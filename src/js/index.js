b8f169039d35b6897963fb964837baa163fb2496
José Marcos <126832016+josemarcosprado@users.noreply.github.com>
2024-04-14 23:39:53 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,17 @@
+const botao = document.querySelector('.btn-mostrar-projetos');
+const projetosInativos = document.querySelectorAll('.projeto:not(.ativo)')
+
+botao.addEventListener('click', () => {
+    mostrarMaisProjetos();
+    esconderBotao();
+});
+
+function esconderBotao() {
+    botao.classList.add('remover');
+}
+
+function mostrarMaisProjetos() {
+    projetosInativos.forEach(projetoInativo => {
+        projetoInativo.classList.add('ativo');
+    });
+}
